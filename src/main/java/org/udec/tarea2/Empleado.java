f17a57facbedbf29dd56895b5f3dd01ef5a56f63
byNoRx <norxgamer12@gmail.com>
2025-05-21 23:23:35 -0400

    Añadir setters

--- src/main/java/org/udec/tarea2/Empleado.java
+++ src/main/java/org/udec/tarea2/Empleado.java
@@ -17,16 +17,32 @@ public class Empleado implements Invitable {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getApellidos() {
         return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getCorreo() {
         return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
     @Override
