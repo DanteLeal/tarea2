7b1b76843a6729003cf2d90dd9df8c3305a68df0
byNoRx <norxgamer12@gmail.com>
2025-05-22 14:30:49 -0400

    Actualizar documentación

--- src/main/java/org/udec/tarea2/Invitable.java
+++ src/main/java/org/udec/tarea2/Invitable.java
@@ -1,5 +1,12 @@
 package org.udec.tarea2;
 
+/**
+ * Interfaz Invitable que define el método invitar
+ *  
+ */
 public interface Invitable {
+    /*
+     * Método para invitar a un empleado
+     */
     public void invitar();
 }
