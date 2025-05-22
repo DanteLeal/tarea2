5d9c20176e1fafd8e1bd29121bacb59cf3efa5d2
byNoRx <norxgamer12@gmail.com>
2025-05-22 13:32:57 -0400

    Añadir parámetro sala en el constructor, setters y getters

--- src/main/java/org/udec/tarea2/ReunionPresencial.java
+++ src/main/java/org/udec/tarea2/ReunionPresencial.java
@@ -7,9 +7,18 @@ import java.util.Date;
 public class ReunionPresencial extends Reunion{
     private String sala;
 
-    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, Instant horaInicio, Instant horaFin) {
+    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, Instant horaInicio, Instant horaFin, String sala) {
         super(fecha, horaPrevista, duracionPrevista, horaInicio, horaFin);
+        this.sala = sala;
     }
 
+    // Getters y setters
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
 
 }
