5f542de08667c1c9ffee3b91d3eebe8ce0aa0207
byNoRx <norxgamer12@gmail.com>
2025-05-22 21:55:59 -0400

    Añadir atributo asistente y su getter y setter

--- src/main/java/org/udec/tarea2/Asistencia.java
+++ src/main/java/org/udec/tarea2/Asistencia.java
@@ -1,6 +1,22 @@
 package org.udec.tarea2;
 
+import java.util.ArrayList;
+
 public class Asistencia {
-    public Asistencia() {
+    
+    Empleado asistente;
+
+    public Asistencia(Empleado empleado) {
+        this.asistente = empleado;
+    }
+
+    // Getters y Setters
+
+    public Empleado getAsistente() {
+        return asistente;
+    }
+
+    public void setAsistente(Empleado asistente) {
+        this.asistente = asistente;
     }
 }
