87978ad40b95b0723f2a7d96e0f2eeff937f4c02
byNoRx <norxgamer12@gmail.com>
2025-05-22 22:15:02 -0400

    Añadir atributo invitable, getters, setters, calcular la hora al construir, documentar

--- src/main/java/org/udec/tarea2/Invitacion.java
+++ src/main/java/org/udec/tarea2/Invitacion.java
@@ -2,10 +2,38 @@ package org.udec.tarea2;
 
 import java.time.Instant;
 
+/**
+ * Clase Invitacion que representa una invitación a una reunión
+ */
 public class Invitacion {
+    private Invitable invitable;
     private Instant hora;
 
-    public Invitacion(Instant hora) {
+    /**
+     * Constructor de la clase Invitacion
+     * 
+     * @param invitable empleado a invitar
+     */
+    public Invitacion(Invitable invitable) {
+        this.invitable = invitable;
+        this.hora = Instant.now();
+    }
+
+    // Getters y setters
+
+    public Invitable getInvitable() {
+        return invitable;
+    }
+
+    public void setInvitable(Invitable invitable) {
+        this.invitable = invitable;
+    }
+
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
         this.hora = hora;
     }
 }
