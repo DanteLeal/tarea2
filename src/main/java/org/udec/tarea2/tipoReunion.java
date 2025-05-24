2a386f0ba483e63c4fd5b2ee39d7dc56d20b4442
byNoRx <norxgamer12@gmail.com>
2025-05-23 20:34:33 -0400

    Los tipos de reunion ahora tienen id y descripción, además se añadieron getters y setters al enum

--- src/main/java/org/udec/tarea2/tipoReunion.java
+++ src/main/java/org/udec/tarea2/tipoReunion.java
@@ -4,12 +4,44 @@ package org.udec.tarea2;
  * Clase Enum que representa los tipos de reuniones
  */
 public enum tipoReunion {
-    TECNICA,
-    MARKETING,
-    OTRO;
+    TECNICA("1", "Reunión técnica enfocada en aspectos de desarrollo o infraestructura"),
+    MARKETING("2", "Reunión para discutir estrategias y acciones de marketing"),
+    OTRO("3", "Otro tipo de reunión no clasificada");
 
+    private String id;
+    private String descripcion;
+
+    /**
+     * Constructor
+     * 
+     * @param id
+     * @param descripcion
+     */
+    tipoReunion(String id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    
     @Override
     public String toString() {
         return "Clase Enum que representa los tipos de reuniones";
+    }
+
+    // Getters y Setters
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
