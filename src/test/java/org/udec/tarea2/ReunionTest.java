90600ac49e100760cbd61665c55a9d5ccecdb457
byNoRx <norxgamer12@gmail.com>
2025-05-23 23:35:29 -0400

    Añadir esqueleto de los tests de Departamento, Invitable, Reunion

---
+++ src/test/java/org/udec/tarea2/ReunionTest.java
@@ -0,0 +1,82 @@
+package org.udec.tarea2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReunionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void obtenerAsistencias() {
+    }
+
+    @Test
+    void obtenerAusencias() {
+    }
+
+    @Test
+    void obtenerRetrasos() {
+    }
+
+    @Test
+    void obtenerTotalAsistencia() {
+    }
+
+    @Test
+    void obtenerPorcentajeAsistencia() {
+    }
+
+    @Test
+    void calcularTiempoReal() {
+    }
+
+    @Test
+    void iniciar() {
+    }
+
+    @Test
+    void finalizar() {
+    }
+
+    @Test
+    void addNota() {
+    }
+
+    @Test
+    void removeNota() {
+    }
+
+    @Test
+    void obtenerNotas() {
+    }
+
+    @Test
+    void addInvitacion() {
+    }
+
+    @Test
+    void obtenerInvitaciones() {
+    }
+
+    @Test
+    void addAsistencia() {
+    }
+
+    @Test
+    void addRetraso() {
+    }
+
+    @Test
+    void elaborarInforme() {
+    }
+}
