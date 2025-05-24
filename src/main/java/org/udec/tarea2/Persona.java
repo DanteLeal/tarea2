adab41db8a367bb10fce880dcdaeb08f59ae7d49
byNoRx <norxgamer12@gmail.com>
2025-05-23 23:12:27 -0400

    Documentar y añadir override de toString

--- src/main/java/org/udec/tarea2/Persona.java
+++ src/main/java/org/udec/tarea2/Persona.java
@@ -1,11 +1,22 @@
 package org.udec.tarea2;
 
+/**
+ * Clase Persona que representa a una persona que puede asistir a una reunión
+ */
 public class Persona implements Invitable {
     private String id;
     private String nombre;
     private String apellidos;
     private String correo;
 
+    /**
+     * Constructor
+     * 
+     * @param id
+     * @param nombre
+     * @param apellidos
+     * @param correo
+     */
     public Persona (String id, String nombre, String apellidos, String correo) {
         this.id = id;
         this.nombre = nombre;
@@ -13,9 +24,21 @@ public class Persona implements Invitable {
         this.correo = correo;
     }
 
+    /**
+     * Invita a la persona a la reunión
+     * 
+     * @param reunion La reunión a la que se invita la persona
+     */
     public void invitar(Reunion reunion) {
         System.out.println("Se ha invitado a la persona " + this.nombre + " " + this.apellidos);
         reunion.addInvitacion(this);
+    }
+
+    // Getters y Setters
+
+    @Override
+    public String toString() {
+        return "Clase Persona que representa a una persona que puede asistir a una reunión";
     }
 
     public String getId() {
